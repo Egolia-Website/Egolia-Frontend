4e417f1e4a2284947fd4dbef7837f6d1f91be53d
Anik246 <shahriarr246@gmail.com>
2026-04-23 18:45:17 -0400

    updated

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
             alt="Egolia Group"
             width={180}
             height={50}
-            className="object-contain mix-blend-multiply w-[120px] sm:w-[145px] lg:w-[165px] h-[58px]"
+            className="object-contain mix-blend-multiply w-[150px] sm:w-[175px] lg:w-[200px] h-[58px]"
             priority
           />
         </Link>
